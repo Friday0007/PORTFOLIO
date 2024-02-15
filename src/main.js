38307f02252608c711a9eb51da632ac29ffe7520
Vaibhav <vaimistry24@gmail.com>
2024-02-15 23:21:41 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '../styles/style.css';
+import '../styles/normailze-css.css';
+import '../styles/header.css';
+import '../styles/page1.css';
+import '../styles/about.css';
+import '../styles/projects.css';
+import '../styles/liveProjects.css';
+import '../styles/contact.css';
+import '../styles/footer.css';
+
+import '../styles/utils.css';
